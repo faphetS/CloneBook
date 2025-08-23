5c0c2f8d70c0b468a57445b684a0d34b736c4af4
faphetS <serjeog0210@gmail.com>
2025-08-23 20:20:18 +0800

    feat: database connection

---
+++ Server/config/db.ts
@@ -0,0 +1,36 @@
+import mysql from "mysql2/promise";
+
+let pool: mysql.Pool;
+
+export const connDB = async (): Promise<mysql.Pool> => {
+  try {
+    if (!pool) {
+      pool = mysql.createPool({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: Number(process.env.DB_PORT) || 3309,
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0,
+      });
+      console.log("MySQL Connected!");
+    }
+    return pool;
+
+  } catch (error: any) {
+
+    console.error("Error connecting to MySQL:", error.message);
+    process.exit(1);
+
+  }
+};
+
+export const getDB = (): mysql.Pool => {
+  if (!pool) {
+    throw new Error("Database not connected. Call connDB() first.");
+  }
+
+  return pool;
+}
