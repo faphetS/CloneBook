d3e13d7b41ff0e246b20cd5f224281fccb927ae0
faphetS <serjeog0210@gmail.com>
2025-08-24 22:59:18 +0800

    fix: resolve Node crash

--- Server/middleware/authMiddleware.ts
+++ Server/middleware/authMiddleware.ts
@@ -1,6 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import { JwtPayload } from "../types/jwt.types.js";
+import { JwtPayload } from "../types/jwtPayload.types";
+
+declare module "express-serve-static-core" {
+  interface Request {
+    user?: JwtPayload;
+  }
+}
 
 export const authenticateToken = (
   req: Request,
@@ -12,12 +18,17 @@ export const authenticateToken = (
     const token = authHeader && authHeader.split(" ")[1];
     if (!token) return res.sendStatus(401);
 
-    jwt.verify(token, process.env.JWT_ACCESS_SECRET as string, (err, decoded) => {
-      if (err) return res.sendStatus(403);
-      if (typeof decoded === "string") return res.sendStatus(403);
-      req.user = decoded as JwtPayload;
-      next();
-    });
+    jwt.verify(
+      token,
+      process.env.ACCESS_TOKEN_SECRET as string,
+      (err, decoded) => {
+        if (err) return res.sendStatus(403);
+        if (typeof decoded === "string") return res.sendStatus(403);
+
+        req.user = decoded as JwtPayload;
+        next();
+      }
+    );
   } catch (err) {
     console.error("Auth middleware error:", err);
     res.sendStatus(500);
