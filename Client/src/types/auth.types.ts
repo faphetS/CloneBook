0d49b0a85810f4becba37f12fb7bbfbf544d47f5
faphetS <serjeog0210@gmail.com>
2025-08-25 21:35:32 +0800

    feat: add interfaces

---
+++ Client/src/types/auth.types.ts
@@ -0,0 +1,14 @@
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+export interface AuthState {
+  user: User | null;
+  accessToken: string | null;
+  refreshToken: string | null;
+  login: (user: User, accessToken: string, refreshToken: string) => void;
+  logout: () => void;
+  setTokens: (accessToken: string, refreshToken: string) => void;
+}
--- Server/types/jwtPayload.types.ts
+++ Server/types/jwtPayload.types.ts
@@ -2,3 +2,7 @@ export interface JwtPayload {
   userId: number;
   role: string;
 }
+
+export interface refreshTokenPayload {
+  userId: number;
+}
