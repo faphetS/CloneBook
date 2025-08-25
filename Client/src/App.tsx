88fd1aaecb9ec6468dd1a9408abd7d85d5c8c458
faphetS <serjeog0210@gmail.com>
2025-08-25 21:24:21 +0800

    feat: added profile route

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,15 +1,17 @@
 import { Route, Routes } from "react-router-dom"
-import HomePage from "./pages/HomePage"
+import HomePage from "./pages/HomePage/HomePage"
 import Login from "./pages/Login"
+import ProfilePage from "./pages/ProfilePage"
 import Signup from "./pages/Signup"
 
 const App = () => {
   return (
-    <div className=" font-montserrat text-black text-sm min-h-screen">
+    <div className="bg-neutral-800 font-montserrat text-white text-sm min-h-screen">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
     </div>
   )
