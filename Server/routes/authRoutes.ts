4c1a211b8330deae2b0fd6c32f6ca322c982cdd1
faphetS <serjeog0210@gmail.com>
2025-08-25 21:32:42 +0800

    feat: add logout and refresh token route

--- Server/routes/authRoutes.ts
+++ Server/routes/authRoutes.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import { login, signup } from "../controllers/user.controller.js";
+import { login, logout, refreshToken, signup } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.post("/logout", logout);
+router.post("/refresh-token", refreshToken);
 
 export default router;
