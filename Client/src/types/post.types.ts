0b5dd7ba65c2a15a6ef6f780999d1ed05fca9d9f
faphetS <serjeog0210@gmail.com>
2025-08-26 21:22:27 +0800

    feat: add post types

---
+++ Client/src/types/post.types.ts
@@ -0,0 +1,12 @@
+
+export interface PostType {
+  id: number;
+  username: string;
+  content: string;
+  created_at: string;
+};
+export interface PostProps {
+  username: string;
+  content: string;
+  created_at: string;
+};
