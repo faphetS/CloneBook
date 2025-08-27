ed9cf7313f471af7730c5202f34e90bf516f7ef5
faphetS <serjeog0210@gmail.com>
2025-08-27 21:41:01 +0800

    feat: implementing like post feature

--- Client/src/components/Posts/Post.tsx
+++ Client/src/components/Posts/Post.tsx
@@ -1,8 +1,24 @@
 import { formatDistanceToNow } from "date-fns";
+import { useState } from "react";
+import api from "../../api/axios";
 import type { PostProps } from "../../types/post.types";
 
 //  post text only
-const Post = ({ username, content, created_at }: PostProps) => {
+const Post = ({ id, username, content, created_at, likeCount: initialLikeCount, isLiked }: PostProps) => {
+
+  const [liked, setLiked] = useState(isLiked);
+  const [likeCount, setLikeCount] = useState(initialLikeCount);
+
+
+  const handleLike = async (): Promise<void> => {
+    try {
+      const res = await api.post(`/content/post/${id}`);
+      setLiked(Boolean(res.data.liked));
+      setLikeCount((prev) => (res.data.liked ? prev + 1 : prev - 1));
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <div className="bg-neutral-900 w-full flex flex-col items-center rounded-2xl px-3 pt-3 gap-3">
 
@@ -26,18 +42,27 @@ const Post = ({ username, content, created_at }: PostProps) => {
           />
         </svg>
         <div className="flex flex-col">
-          <p>{username}</p>
+          <p className="font-semibold">{username}</p>
           <p className="text-neutral-400 text-xs">
             {formatDistanceToNow(new Date(created_at), { addSuffix: true })}
           </p>
         </div>
       </div>
-      <p className="text-white text-base whitespace-pre-wrap break-words max-w-[590px] w-full">
+      <p className="text-white font-base whitespace-pre-wrap break-words max-w-[640px] w-full">
         {content}
       </p>
+      <div className="flex justify-between w-full  max-w-[640px] text-xs text-neutral-400">
+        <div>{likeCount > 0
+          ? `${likeCount} like${likeCount > 1 ? "s" : ""}`
+          : ""}
+        </div>
+        <div>5 comments</div>
+      </div>
       <div className="flex justify-around items-center border-t border-neutral-700 w-full py-2">
-        <button className="hover:bg-neutral-700/50  hover:text-white w-full py-2 rounded-md cursor-pointer text-neutral-300 font-semimbold transition duration-100">Like</button>
-        <button className="hover:bg-neutral-700/50 hover:text-white w-full py-2 rounded-md cursor-pointer text-neutral-300 font-semimbold transition duration-100">Comment</button>
+        <button onClick={handleLike} className={`hover:bg-neutral-700/50 w-full py-2 rounded-md cursor-pointer  font-semibold transition duration-100 ${liked ? "text-[#1877F2]" : "text-neutral-400"}`}>
+          {liked ? "Liked" : "Like"}
+        </button>
+        <button className="hover:bg-neutral-700/50 w-full py-2 rounded-md cursor-pointer text-neutral-400 font-semibold transition duration-100">Comment</button>
       </div>
 
     </div>
