e216a05a98aef226ba6c93ee5ee7138a80655090
faphetS <serjeog0210@gmail.com>
2025-08-27 21:45:16 +0800

    feat: add toggleLike route path

--- Server/routes/postsRoutes.ts
+++ Server/routes/postsRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createPost, getPosts } from "../controllers/posts.controller.js";
+import { createPost, getPosts, toggleLike } from "../controllers/posts.controller.js";
 import { authenticateToken } from "../middleware/authMiddleware.js";
 
 const router = Router();
 
-router.get("/", getPosts);
+router.get("/", authenticateToken, getPosts);
 router.post("/post", authenticateToken, createPost);
+router.post("/post/:postId", authenticateToken, toggleLike);
 
 export default router;
