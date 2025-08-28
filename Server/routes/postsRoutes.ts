126412045155ebe822d7f287992b39db7d1930bf
faphetS <serjeog0210@gmail.com>
2025-08-28 23:28:41 +0800

    feat: route for get/fetch comment

--- Server/routes/postsRoutes.ts
+++ Server/routes/postsRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createPost, getPosts, toggleLike } from "../controllers/posts.controller.js";
+import { createPost, getComments, getPosts, postComment, toggleLike } from "../controllers/posts.controller.js";
 import { authenticateToken } from "../middleware/authMiddleware.js";
 
 const router = Router();
@@ -7,5 +7,7 @@ const router = Router();
 router.get("/", authenticateToken, getPosts);
 router.post("/post", authenticateToken, createPost);
 router.post("/post/:postId", authenticateToken, toggleLike);
+router.get("/post/:postId/comments", authenticateToken, getComments);
+router.post("/post/:postId/comments", authenticateToken, postComment);
 
 export default router;
