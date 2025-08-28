0a3f4fdab913c37c8260cad3a99424fa8e2f4e8e
faphetS <serjeog0210@gmail.com>
2025-08-28 23:28:08 +0800

    feat: new time format

---
+++ Client/src/utils/time.ts
@@ -0,0 +1,15 @@
+import { differenceInDays, differenceInHours, differenceInMinutes } from "date-fns";
+
+export function formatShortTime(date: Date) {
+  const now = new Date();
+
+  const minutes = differenceInMinutes(now, date);
+  if (minutes < 1) return "now"; // optional for <1m
+  if (minutes < 60) return `${minutes}m`;
+
+  const hours = differenceInHours(now, date);
+  if (hours < 24) return `${hours}h`;
+
+  const days = differenceInDays(now, date);
+  return `${days}d`;
+}
