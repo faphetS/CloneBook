110dfa5f678b7db9f01da3dae48867eb70816884
faphetS <serjeog0210@gmail.com>
2025-08-29 20:17:51 +0800

    feat: made a commentStore

---
+++ Client/src/store/commentStore.ts
@@ -0,0 +1,75 @@
+import { create } from "zustand";
+import api from "../api/axios";
+import type { CommentState, CommentType } from "../types/comment.types";
+
+export const useCommentStore = create<CommentState>((set, get) => ({
+  comments: {},
+  loading: {},
+
+  setComments: (postId: number, comments: CommentType[]) => {
+    set((state) => ({
+      comments: {
+        ...state.comments,
+        [postId]: comments,
+      },
+    }));
+  },
+
+
+  fetchComments: async (postId: number) => {
+    if (get().loading[postId]) return; // prevent duplicate requests
+    set((state) => ({
+      loading: { ...state.loading, [postId]: true },
+    }));
+
+    try {
+      const res = await api.get(`/content/post/${postId}/comments`);
+      const comments: CommentType[] = res.data.map((c: CommentType) => ({
+        ...c,
+        isLiked: Boolean(c.isLiked),
+        likeCount: c.likeCount ?? 0,
+      }));
+      get().setComments(postId, comments);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      set((state) => ({
+        loading: { ...state.loading, [postId]: false },
+      }));
+    }
+  },
+
+  addComment: (postId: number, comment: CommentType) => {
+    const formatted: CommentType = {
+      ...comment,
+      likeCount: comment.likeCount ?? 0,
+      isLiked: Boolean(comment.isLiked),
+    };
+
+    set((state) => ({
+      comments: {
+        ...state.comments,
+        [postId]: [formatted, ...(state.comments[postId] ?? [])],
+      },
+    }));
+  },
+
+  toggleLike: (postId: number, commentId: number) => {
+    set((state) => ({
+      comments: {
+        ...state.comments,
+        [postId]: (state.comments[postId] || []).map((c) =>
+          c.id === commentId
+            ? {
+              ...c,
+              isLiked: !c.isLiked,
+              likeCount: c.isLiked ? c.likeCount - 1 : c.likeCount + 1,
+            }
+            : c
+        ),
+      },
+    }));
+  }
+
+
+}));
