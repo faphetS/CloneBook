31006174a59e0a800d857634da95213693e04750
faphetS <serjeog0210@gmail.com>
2025-08-31 20:05:20 +0800

    feat: route for userRoutes

--- Server/routes/index.ts
+++ Server/routes/index.ts
@@ -2,11 +2,13 @@ import express from "express";
 import authRoutes from "./authRoutes.js";
 import postsRoutes from "./postsRoutes.js";
 import protectedRoutes from "./protectedRoutes.js";
+import userRoutes from "./userRoutes.js";
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
 router.use("/protected", protectedRoutes);
 router.use("/content", postsRoutes);
+router.use("/user", userRoutes);
 
 export default router;
