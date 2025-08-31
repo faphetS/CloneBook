f7eb3a86e053e7f096bf4157ef9b93a56b2715fe
faphetS <serjeog0210@gmail.com>
2025-08-31 19:51:00 +0800

    refactor: rename useAuthStore.ts to AuthStore.ts

--- Client/src/api/axios.ts
+++ Client/src/api/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useAuthStore } from "../store/useAuthStore";
+import { useAuthStore } from "../store/AuthStore";
 
 const api = axios.create({
   baseURL: "/api",
