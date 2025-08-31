5b8603d0e05df314a132d880735d20f30742e418
faphetS <serjeog0210@gmail.com>
2025-08-31 20:02:53 +0800

    feat: made /public folder available

--- Server/server.ts
+++ Server/server.ts
@@ -2,10 +2,14 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import express, { Application, Request, Response } from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 import { connDB } from "./config/db.js";
 import apiRoutes from "./routes/index.js";
 
 dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app: Application = express();
 const PORT: number = parseInt(process.env.PORT || "3000", 10);
 
@@ -16,10 +20,13 @@ app.use(cors({
   origin: "http://localhost:5173",
   credentials: true
 }));
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("api test running...");
 });
+
+
 
 app.use("/api", apiRoutes);
 
