1cfe03dde52b64e7e0045194baa350ca2e3a24cf
faphetS <serjeog0210@gmail.com>
2025-09-02 19:26:50 +0800

    feat: controller for notification

---
+++ Server/controllers/notif.controller.ts
@@ -0,0 +1,65 @@
+import { Request, Response } from "express";
+import { getDB } from "../config/db.js";
+
+//helper
+export const createNotification = async (
+  userId: number,
+  senderId: number,
+  type: string,
+  text: string
+) => {
+  const query = `INSERT INTO notifications (user_id, sender_id, type, text, unread) 
+                 VALUES (?, ?, ?, ?, 1)`;
+  const db = getDB();
+  await db.execute(query, [userId, senderId, type, text]);
+};
+
+export const getNotif = async (req: Request, res: Response) => {
+  if (!req.user) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  const query = `
+      SELECT 
+        n.id, 
+        n.type, 
+        n.text, 
+        n.unread, 
+        n.created_at AS createdAt, 
+        u.username AS senderName,
+        u.profile_pic AS profilePic
+      FROM notifications n
+      JOIN users u ON n.sender_id = u.id
+      WHERE n.user_id = ?
+      ORDER BY n.created_at DESC
+    `;
+
+  try {
+    const db = getDB();
+    const [rows]: any = await db.execute(query, [req.user.userId]);
+    res.json(rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const markAllAsRead = async (req: Request, res: Response) => {
+  if (!req.user) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  const query = "UPDATE notifications SET unread = 0 WHERE user_id = ?";
+
+  try {
+    const db = getDB();
+    db.execute(query, [req.user.userId]);
+    res.json({ message: "All notifications marked as read" });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to mark notifications as read" });
+  }
+};
+
+
+
+
