3b32c50f1502599caa8808aaf19343caa36d9345
faphetS <serjeog0210@gmail.com>
2025-09-03 22:08:08 +0800

    style: design polish

--- Client/src/components/AccountSettings.tsx
+++ Client/src/components/AccountSettings.tsx
@@ -1,12 +1,14 @@
 import { Menu, MenuButton, MenuItem } from '@headlessui/react'
 import { AnimatePresence, motion } from 'framer-motion'
+
+
 const AccountSettings = () => {
   return (
 
     <Menu as="nav" className="relative">
       {({ open }) => (
         <>
-          <MenuButton className="flex items-center px-4 h-12 hover:bg-neutral-700/50 cursor-pointer rounded-lg mt-1 w-full text-left focus:outline-none z-10">
+          <MenuButton className="flex items-center pl-[19px] h-12 hover:bg-neutral-700/50 cursor-pointer rounded-lg mt-1 w-full text-left focus:outline-none z-10">
             <motion.svg
               animate={{ rotate: open ? 90 : 0 }}
               transition={{ duration: 0.2 }}
@@ -15,11 +17,11 @@ const AccountSettings = () => {
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="currentColor"
-              className="w-6 h-6 text-neutral-400 mr-4"
+              className="w-6 h-6 text-neutral-400 mr-[17px]"
             >
               <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
             </motion.svg>
-            Account Settings
+            Settings
           </MenuButton>
 
           <AnimatePresence>
@@ -33,11 +35,11 @@ const AccountSettings = () => {
               >
                 <MenuItem>
                   <div>
-                    {['Edit Profile Details', 'Change Profile Photo', 'Change Password'].map((label) => (
+                    {['Edit Profile Details', 'Logout'].map((label) => (
                       <MenuItem
                         key={label}
                         as="div"
-                        className="cursor-pointer flex items-center pl-[54px] h-12 rounded-md text-neutral-200 hover:bg-neutral-700/50"
+                        className="cursor-pointer flex items-center pl-[60px] h-12 rounded-md text-neutral-200 hover:bg-neutral-700/50"
                       >
                         {label}
                       </MenuItem>
