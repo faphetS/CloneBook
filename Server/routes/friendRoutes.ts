16e441dd73ef6fc5e5211b7f3a2b4019e488bfae
faphetS <serjeog0210@gmail.com>
2025-09-06 21:17:41 +0800

    feat: route for unfriendUser

--- Server/routes/friendRoutes.ts
+++ Server/routes/friendRoutes.ts
@@ -7,22 +7,23 @@ import {
   getFriendRequests,
   getFriends,
   getFriendStatus,
-  sendFriendRequest
+  sendFriendRequest,
+  unfriendUser
 } from "../controllers/friend.controller.js";
 import { authenticateToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-// Requests
-router.post("/request", authenticateToken, sendFriendRequest);
 router.get("/requests", authenticateToken, getFriendRequests);
+router.get("/count/:userId", getFriendCount);
+router.get("/status/:userId", authenticateToken, getFriendStatus);
+router.get("/", authenticateToken, getFriends);
+
+router.post("/request", authenticateToken, sendFriendRequest);
 router.post("/request/accept", authenticateToken, acceptFriendRequest);
 router.post("/request/decline", authenticateToken, declineFriendRequest);
 router.post("/request/cancel", authenticateToken, cancelFriendRequest);
 
-// Friends
-router.get("/count/:userId", getFriendCount);
-router.get("/status/:userId", authenticateToken, getFriendStatus);
-router.get("/", authenticateToken, getFriends);
+router.delete("/:friendId", authenticateToken, unfriendUser)
 
 export default router;
