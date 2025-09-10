52fe8a1c726b48bb2ac65c7894ee4e6881ee0f76
faphetS <serjeog0210@gmail.com>
2025-09-10 21:24:44 +0800

    feat: made new types and edit some to support pagination

--- Client/src/types/friend.types.ts
+++ Client/src/types/friend.types.ts
@@ -4,7 +4,12 @@ export type Friend = {
   profilePic: string | null;
 };
 
-export type FriendStatus = "self" | "friends" | "pending_incoming" | "pending_outgoing" | "none";
+export type FriendStatus =
+  "self"
+  | "friends"
+  | "pending_incoming"
+  | "pending_outgoing"
+  | "none";
 
 export type FriendRequest = {
   id: number;
@@ -13,21 +18,46 @@ export type FriendRequest = {
   senderProfilePic: string | null;
   createdAt: string;
 };
+
+export type FriendPagination = {
+  offset: number;
+  limit: number;
+  hasMore: boolean;
+  loading: boolean;
+};
+
+export type PendingPagination = {
+  offset: number;
+  limit: number;
+  hasMore: boolean;
+  loading: boolean;
+  loadingMore: boolean;
+};
+
 export type FriendStore = {
   friends: Friend[];
+  friendsPagination: FriendPagination;
+
   pendingRequests: FriendRequest[];
+  pendingPagination: PendingPagination;
+
   outgoingRequests: number[];
   friendCount: number;
-  loading: boolean;
+  pendingCount: number;
   friendStatus: FriendStatus;
 
   fetchFriends: () => Promise<void>;
-  fetchPendingRequests: () => Promise<void>;
-  fetchFriendStatus: (userId: number) => Promise<void>;
+  resetFriends: () => void;
   fetchFriendCount: (userId: number) => Promise<void>;
+  fetchFriendStatus: (userId: number) => Promise<void>;
+  unfriend: (friendId: number) => Promise<void>;
+
+  resetPendingReq: () => void;
+  fetchPendingCount: () => Promise<void>
+  fetchPendingRequests: (loadMore?: boolean) => Promise<void>;
+
   sendRequest: (receiverId: number) => Promise<void>;
-  acceptRequest: (senderId: number) => Promise<void>;
+  acceptRequest: (friend: Friend) => Promise<void>;
   declineRequest: (senderId: number) => Promise<void>;
   cancelRequest: (receiverId: number) => Promise<void>;
-  unfriend: (friendId: number) => Promise<void>;
 };
