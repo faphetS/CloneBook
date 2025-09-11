4e9b4748cd2c2026a0215f61335db8173d48dd2a
faphetS <serjeog0210@gmail.com>
2025-09-11 18:43:23 +0800

    feat: made searchInput to its own component and is working

--- Client/src/components/Header.tsx
+++ Client/src/components/Header.tsx
@@ -1,11 +1,8 @@
 import { Link } from 'react-router-dom';
-
 import FriendReq from './FriendReq';
 import NotificationDropdown from './NotificationDropDown';
 import ProfileDropDown from './ProfileDropDown';
-
-
-
+import SearchInput from './SearchInput';
 
 const Header: React.FC = () => {
   return (
@@ -16,28 +13,7 @@ const Header: React.FC = () => {
           <div className="font-bold cursor-pointer">CloneBook</div>
         </Link>
         {/* Search Input */}
-        <div className="relative w-[240px]">
-          <input
-            type="text"
-            placeholder="Search CloneBook"
-            className="w-full bg-neutral-800 text-sm text-white rounded-full pl-10 pr-4 py-2 focus:outline-none"
-          />
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-5 h-5 absolute left-3 top-1/2 -translate-y-1/2 text-neutral-400"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M21 21l-4.35-4.35m0 0A7.5 7.5 0 1 0 5.25 
-                 5.25a7.5 7.5 0 0 0 11.5 11.5z"
-            />
-          </svg>
-        </div>
+        <SearchInput />
       </div>
 
 
