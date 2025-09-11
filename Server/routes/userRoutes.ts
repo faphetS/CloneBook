5cbc4201d014ec44abc0514ad8f9fceb003d5a88
faphetS <serjeog0210@gmail.com>
2025-09-11 18:43:58 +0800

    feat: implement search feature

--- Server/routes/userRoutes.ts
+++ Server/routes/userRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { getUserInfo, updateProfile } from "../controllers/user.controller.js";
+import { getUserInfo, searchUser, updateProfile } from "../controllers/user.controller.js";
 import { authenticateToken } from "../middleware/authMiddleware.js";
 import { upload } from "../middleware/upload.js";
 
 const router = Router();
 
-router.get("/:userId", authenticateToken, getUserInfo);
+router.get("/search", searchUser);
 router.put("/profile", authenticateToken, upload.single("profilePic"), updateProfile);
+router.get("/:userId", authenticateToken, getUserInfo);
 
 export default router;
