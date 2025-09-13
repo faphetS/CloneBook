32ad63c53a122f011be7e13b995348075303727f
faphetS <serjeog0210@gmail.com>
2025-09-13 19:09:56 +0800

    feat: new button when in phone

--- Client/src/components/ProfileDropDown.tsx
+++ Client/src/components/ProfileDropDown.tsx
@@ -95,6 +95,19 @@ const ProfileDropDown = () => {
               >
                 <div className="flex flex-col">
 
+                  <MenuItem as={Link} to={`/`} className="px-2 py-2 hover:bg-neutral-800/60 cursor-pointer flex items-center gap-2 rounded-xl block sm:hidden">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="size-9 p-1 transition-colors duration-300 bg-neutral-800 text-neutral-400 rounded-full">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                    </svg>
+                    Home
+                  </MenuItem>
+
                   <MenuItem as={Link} to={`/profile/${user?.id}`} className="px-2 py-2 hover:bg-neutral-800/60 cursor-pointer flex items-center gap-2 rounded-xl">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
