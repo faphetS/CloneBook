e56864105a72ac516165c0a5a5664e4d5834eaef
faphetS <serjeog0210@gmail.com>
2025-09-15 20:37:44 +0800

    style: fix alignment

--- Client/src/components/EditProfileModal.tsx
+++ Client/src/components/EditProfileModal.tsx
@@ -72,7 +72,7 @@ const EditProfileModal = ({ isOpen, closeModal, onSave, currentUsername, current
               (<div className="bg-red-500/20 border border-red-500/50 text-red-200 px-2 py-2 rounded-md text-sm text-center">{error}</div>)
               :
               (
-                <h2 className="text-xl font-semibold text-white text-center">Edit Profile</h2>
+                <h2 className="text-xl font-semibold text-white text-center px-2 py-2">Edit Profile</h2>
               )}
 
 
@@ -101,34 +101,42 @@ const EditProfileModal = ({ isOpen, closeModal, onSave, currentUsername, current
             </div>
 
             <div className="flex justify-end gap-2 mt-4">
-              <button onClick={handleCancel} className="px-4 py-2 rounded-md bg-neutral-700 hover:bg-neutral-600 text-white transition">Cancel</button>
-              <button onClick={handleSave} disabled={!!error || loading} className="px-4 py-2 rounded-md bg-[#1877F2] disabled:bg-[#3a8cff]/50 hover:bg-[#3a8cff] text-white transition">
-                {loading ?
-                  (
-                    <svg
-                      className="animate-spin h-5 w-5 mr-2 text-white"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                    >
-                      <circle
-                        className="opacity-25"
-                        cx="12"
-                        cy="12"
-                        r="10"
-                        stroke="currentColor"
-                        strokeWidth="4"
-                      />
-                      <path
-                        className="opacity-75"
-                        fill="currentColor"
-                        d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
-                      />
-                    </svg>
-                  )
-                  :
-                  ("Save")}
+
+              <button
+                onClick={handleCancel}
+                className="px-4 py-2 rounded-md bg-neutral-700 hover:bg-neutral-600 text-white transition">Cancel</button>
+
+              <button
+                onClick={handleSave}
+                disabled={!!error || loading}
+                className="relative px-4 py-2 rounded-md bg-[#1877F2] disabled:bg-[#3a8cff]/50 hover:bg-[#3a8cff] text-white transition flex justify-center items-center"
+              >
+                {loading ? (
+                  <svg
+                    className="animate-spin h-5 w-5 text-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                    />
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+                    />
+                  </svg>
+                ) : (
+                  "Save"
+                )}
               </button>
+
             </div>
           </motion.div>
         </div>
@@ -138,3 +146,4 @@ const EditProfileModal = ({ isOpen, closeModal, onSave, currentUsername, current
 };
 
 export default EditProfileModal;
+
