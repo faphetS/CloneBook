c15ba3d7bc57cd5f0ef37921134f568af674569f
faphetS <serjeog0210@gmail.com>
2025-09-20 17:03:16 +0800

    fix: removed cookie. set-cookie on allowedheaders

--- Server/server.ts
+++ Server/server.ts
@@ -24,7 +24,7 @@ app.use(cors({
   origin: process.env.FRONTEND_URL,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "Cookie", "Set-Cookie"]
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 
